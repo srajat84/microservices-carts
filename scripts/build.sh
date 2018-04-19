fb1e66da648d165d735a9f9eb69810cf1728eb37
srajattw <srajat@thoughtworks.com>
2018-04-19 16:23:48 +0530

    |Rajat, Ram| Removing GROUP

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,11 +7,6 @@ export BUILD_DATE=`date +%Y-%m-%dT%T%z`
 
 SCRIPT_DIR=$(dirname "$0")
 
-if [[ -z "$GROUP" ]] ; then
-    echo "Cannot find GROUP env var"
-    exit 1
-fi
-
 if [[ -z "$COMMIT" ]] ; then
     echo "Cannot find COMMIT env var"
     exit 1
@@ -28,11 +23,8 @@ $DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /us
 
 cp $CODE_DIR/target/*.jar $CODE_DIR/docker/carts
 
-for m in ./docker/*/; do
-    REPO=${GROUP}/$(basename $m)
     $DOCKER_CMD build \
       --build-arg BUILD_VERSION=$BUILD_VERSION \
       --build-arg BUILD_DATE=$BUILD_DATE \
       --build-arg COMMIT=$COMMIT \
       -t ${REPO}:${COMMIT} $CODE_DIR/$m;
-done;
